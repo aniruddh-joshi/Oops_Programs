519803d88dad0aec7432d4013f7646c3d01fe238
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2022-12-26 19:16:03 +0530

    Friend Function

---
+++ Friend_Function.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class student{
+    private:
+    int rn,m1,m2;
+    public:
+    void set(int x,int y,int z);
+    void get();
+    friend void add(student po);
+};
+void student::set(int x,int y,int z)
+{
+    rn=x;
+    m1=y;
+    m2=z;
+}
+void student::get()
+{
+    cout<<"Roll NO: "<<rn<<endl<<"Marks 1:"<<m1<<endl<<"Marks 2:"<<m2<<endl;
+}
+void add(student po)
+{
+    cout<<"Total Marks is "<<po.m1+po.m2<<endl;
+}
+int main(){
+    student obj1;
+    obj1.set(1,90,90);
+    obj1.get();
+    add(obj1);
+}
