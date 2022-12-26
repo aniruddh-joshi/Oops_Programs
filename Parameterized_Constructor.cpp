fbc036e8a10e0b305f7446b6605a1c842877d7ec
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2022-12-26 19:41:58 +0530

    Parameterized Constructor

---
+++ Parameterized_Constructor.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class student{
+    private:
+    int rn,m1,m2;
+    public:
+    void set(int x,int y,int z);
+    void get();
+    void add();
+    student(int x,int y,int z);
+};
+void student::set(int x,int y, int z)
+{
+    rn=x;
+    m1=y;
+    m2=z;
+}
+void student::get()
+{
+    cout<<"Roll No: "<<rn<<endl<<"Marks 1:"<<m1<<endl<<"Marks 2:"<<m2<<endl;
+}
+void student::add()
+{
+    cout<<"Total Marks is "<<m1+m2<<endl;;
+}
+student::student(int x,int y,int z)
+{
+    cout<<"Parameterized Constructor is called "<<endl;
+    rn=x;
+    m1=y;
+    m2=z;
+}
+int main()
+{
+    student obj1(1,20,30);
+    obj1.get();
+    obj1.add();
+}
