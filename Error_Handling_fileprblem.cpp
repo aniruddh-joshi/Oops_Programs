8a77bfafcaaa2e6b69b4a0d98be65fe1b16ca005
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-01-05 19:48:23 +0530

    Error Handling when file can't Open

---
+++ Error_Handling_fileprblem.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    ifstream myin("file.txt");
+    try{
+        if(!myin) throw 100;
+        char words[255];
+        myin.read(words,sizeof(words));
+        cout<<words;
+        myin.close();
+    }
+    catch(int x)
+    {
+        cout<<"Can't Open File\n";
+        cout<<"Error Code"<<x;
+    }
+}
