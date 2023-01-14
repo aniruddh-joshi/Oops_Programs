22870b5699c3a28f975bc496fb668a42ef4e0eab
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-01-14 19:09:43 +0530

    Number Pattern Problem

---
+++ Number_Pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,row,col;
+    int count=1;
+    cout<<"Enter the number of rows and column you want to enter\n";
+    cin>>row>>col;
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            cout<<count;
+            count++;
+        }
+        cout<<endl;
+    }
+}
